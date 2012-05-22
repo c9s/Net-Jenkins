466e54a4a41b143931bc9d83bab8cee58539c373
c9s <cornelius.howl@gmail.com>
2012-05-23 00:28:07 +0800

    First commit

---
+++ lib/Net/Jenkins.pm
@@ -0,0 +1,43 @@
+package Net::Jenkins;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+use Moose;
+
+has protocol => ( is => 'rw', isa => 'Str', default => 'http' );
+
+has host => ( is => 'rw', isa => 'Str' , default => 'localhost' ) ;
+
+has port => ( is => 'rw', isa => 'Int' , default => 8080 ) ;
+
+
+
+
+
+1;
+__END__
+
+=head1 NAME
+
+Net::Jenkins -
+
+=head1 SYNOPSIS
+
+  use Net::Jenkins;
+
+=head1 DESCRIPTION
+
+Net::Jenkins is
+
+=head1 AUTHOR
+
+Yo-An Lin E<lt>cornelius.howl {at} gmail.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
